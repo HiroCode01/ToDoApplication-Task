c9c5054cdb2a189a055355278c7ee4c7bde3728c
Hiro <abdazimovfazliddin27@gmail.com>
2025-12-16 18:59:51 +0500

    Initial Commit

---
+++ ToDoApplication/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ToDoApplication
+// ContentView.swift
+//
+// Created by HiRO on 16/12/2025
+// Copyright © 2025 ACME. All Rights Reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoApplication/ToDoApplicationApp.swift
@@ -0,0 +1,18 @@
+//
+// ToDoApplication
+// ToDoApplicationApp.swift
+//
+// Created by HiRO on 16/12/2025
+// Copyright © 2025 ACME. All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct ToDoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
